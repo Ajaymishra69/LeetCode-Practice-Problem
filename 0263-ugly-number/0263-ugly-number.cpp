2ebbd8c7e12bd9b90a274da0f517e703b6344b99
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2022-11-18 19:43:22 +0530

    Time: 5 ms (31.29%), Space: 5.9 MB (19.97%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.cpp
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -5,11 +5,11 @@ public:
             return false;
         if(n==1)
             return true;
-       else if(n%2==0)
+       if(n%2==0)
             return isUgly(n/2);
-        else if(n%3==0)
+       if(n%3==0)
             return isUgly(n/3);
-        else if(n%5==0)
+       if(n%5==0)
             return isUgly(n/5);
         return false;
     }
