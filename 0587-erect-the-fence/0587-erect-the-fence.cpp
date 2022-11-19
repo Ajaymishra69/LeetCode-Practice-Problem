9efaf35b322cdee096a9b508b443c77d400b681a
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2022-11-19 23:39:20 +0530

    Time: 242 ms (12.00%), Space: 27.1 MB (15.20%) - LeetHub

--- 0587-erect-the-fence/0587-erect-the-fence.cpp
+++ 0587-erect-the-fence/0587-erect-the-fence.cpp
@@ -1,59 +1,32 @@
-class Solution {
+class Solution 
+{
 public:
-
-    int getRotationAngle(vector<int> A, vector<int> B, vector<int> C) {
-        // (x2-x1)(y3-y1) - (y2-y1)(x3-x1) -> 3D Cross-product of AB and AC vectors
-        return ((B[0] - A[0]) * (C[1] - A[1])) - ((B[1] - A[1]) * (C[0] - A[0]));
-    }
-
-
-    vector<vector<int>> outerTrees(vector<vector<int>>& trees) {
-
-        if (trees.size() <= 3) return trees;
-
+    
+    using tree = vector<int>;
+    
+    vector<tree> outerTrees(vector<tree>& trees) 
+    {
+        auto cross = [](tree& B, tree& A, tree& T) -> int
+        {
+            return (T[1]-B[1])*(B[0]-A[0]) - (B[1]-A[1])*(T[0]-B[0]);
+        };
+        
         sort(trees.begin(), trees.end());
+        
+        deque<tree> F;
+        
+        for (tree T : trees)
+        {
+            while (F.size() >= 2 and cross(F[F.size()-1],F[F.size()-2],T) < 0)
+                F.pop_back();
+            F.push_back(T);
 
-
-        vector<vector<int>> UpperHullTrees;
-
-        UpperHullTrees.push_back(trees[0]);
-        UpperHullTrees.push_back(trees[1]);
-        for (int i = 2; i < trees.size(); i++) {
-            int Us = UpperHullTrees.size();
-
-
-            while (UpperHullTrees.size() >= 2 && getRotationAngle(UpperHullTrees[Us-2], UpperHullTrees[Us-1], trees[i]) > 0)
-   {
-                UpperHullTrees.pop_back();
-                Us--;
-            }
-            UpperHullTrees.push_back(trees[i]);
+            while (F.size() >= 2 and cross(F[0],F[1],T) > 0)
+                F.pop_front();
+            F.push_front(T);
         }
-
-  
-        vector<vector<int>> LowerHullTrees;
-
-        LowerHullTrees.push_back(trees[trees.size() - 1]);
-        LowerHullTrees.push_back(trees[trees.size() - 2]);
-
-        for (int i = trees.size() - 3; i >= 0; --i) {
-
-            int Ls = LowerHullTrees.size();
-
-            while (LowerHullTrees.size() >= 2 && getRotationAngle(LowerHullTrees[Ls-2], LowerHullTrees[Ls-1], trees[i]) > 0) {
-                LowerHullTrees.pop_back();
-                Ls--;
-            }
-            LowerHullTrees.push_back(trees[i]);
-        }
-
-
-          UpperHullTrees.insert( UpperHullTrees.end(), LowerHullTrees.begin(), LowerHullTrees.end() );
-
-
-        sort( UpperHullTrees.begin(), UpperHullTrees.end() );
-        UpperHullTrees.erase( unique( UpperHullTrees.begin(), UpperHullTrees.end() ), UpperHullTrees.end() );
-
-        return UpperHullTrees;
+        
+        set<tree> unique(F.begin(), F.end());
+        return vector<tree>(unique.begin(), unique.end());
     }
 };
