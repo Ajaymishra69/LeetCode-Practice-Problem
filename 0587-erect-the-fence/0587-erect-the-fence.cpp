f55b01259b976faa0f0471b06ef1d876abedc279
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2022-11-19 23:42:02 +0530

    Time: 400 ms (5.60%), Space: 43.7 MB (5.60%) - LeetHub

--- 0587-erect-the-fence/0587-erect-the-fence.cpp
+++ 0587-erect-the-fence/0587-erect-the-fence.cpp
@@ -1,32 +1,75 @@
-class Solution 
-{
+class Solution { // SOLVE USING CONVEX HULL ALGORITHM 
+    
+public: 
+      int getRotate( vector<int>A , vector<int>B , vector<int>C)
+      {
+         return ((B[0]-A[0])*(C[1]-A[1])) - ((B[1]-A[1] )*(C[0]-A[0])) ;  
+      }
 public:
-    
-    using tree = vector<int>;
-    
-    vector<tree> outerTrees(vector<tree>& trees) 
-    {
-        auto cross = [](tree& B, tree& A, tree& T) -> int
+    vector<vector<int>> outerTrees(vector<vector<int>>& trees) {
+        
+        if(trees.size() <= 3)
         {
-            return (T[1]-B[1])*(B[0]-A[0]) - (B[1]-A[1])*(T[0]-B[0]);
-        };
-        
-        sort(trees.begin(), trees.end());
-        
-        deque<tree> F;
-        
-        for (tree T : trees)
-        {
-            while (F.size() >= 2 and cross(F[F.size()-1],F[F.size()-2],T) < 0)
-                F.pop_back();
-            F.push_back(T);
-
-            while (F.size() >= 2 and cross(F[0],F[1],T) > 0)
-                F.pop_front();
-            F.push_front(T);
+            return trees ; 
         }
         
-        set<tree> unique(F.begin(), F.end());
-        return vector<tree>(unique.begin(), unique.end());
+        sort( trees.begin() , trees.end()) ; 
+        
+        // UPPER CONVEX HULL 
+        
+        vector<vector<int>>ltrees ; 
+        
+         ltrees.push_back(trees[0]) ; 
+         ltrees.push_back(trees[1]) ; 
+        
+        for( int i = 2 ; i < trees.size() ; i++)
+        {
+            int ls = ltrees.size() ; 
+            
+            while(ltrees.size() >= 2 && getRotate( ltrees[ls-2] , ltrees[ls-1] , trees[i]) > 0 )
+           {
+               ltrees.pop_back() ; 
+               ls-- ; 
+           }
+         ltrees.push_back(trees[i]) ; 
+        }
+        
+        // LOWER CONVEX HULL 
+        
+        vector<vector<int>>rTrees ; 
+        
+        rTrees.push_back(trees[trees.size()-1]) ; 
+        rTrees.push_back(trees[trees.size()-2]) ; 
+        
+        for( int i = trees.size()-3 ; i >= 0 ; i--)
+        {
+            int rs = rTrees.size() ; 
+            
+            while(rTrees.size() >= 2 && getRotate(rTrees[rs-2] , rTrees[rs-1] , trees[i]) > 0)
+            {
+                rTrees.pop_back() ; 
+                rs-- ; 
+            }
+            
+            rTrees.push_back(trees[i]) ; 
+        }
+        
+        // JOIN UPPER AND LOWER CONVEX HULL 
+        
+        for( int i = 0 ; i < rTrees.size() ; i++)
+        {
+            ltrees.push_back(rTrees[i]) ; 
+        }
+        
+        // REMOVING UNIQUE ELEMENTS 
+        
+        sort(ltrees.begin() , ltrees.end()) ; 
+        
+        ltrees.erase(std::unique(ltrees.begin() , ltrees.end()),ltrees.end()) ; 
+        
+        
+        return ltrees ;
+        
+        
     }
 };
