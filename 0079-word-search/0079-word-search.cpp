783bddbdcc1ded9b27f1b6c9370141d7acc61913
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2022-11-25 04:03:06 +0530

    Time: 1605 ms (11.85%), Space: 8.1 MB (47.00%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,42 +1,90 @@
 class Solution 
 {
 public:
-    
-    int dir[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    
-    bool find(int i, int j, int idx, vector<vector<char>>& board, string word)
+    bool search(int index, int i, int j ,vector<vector<char>>& board, string word)
     {
-        if(idx == word.size()) return true;
+      if(index == word.size())
+      {
+          return true;
+      }
+      if(i<0 or j<0 or i>=board.size() or j>=board[0].size())
+      {
+          return false;
+      }
+      bool ans = false;
+     if(word[index] == board[i][j])
+     {
+         board[i][j] = '*';
+         
+         ans = search(index+1,i+1,j,board,word) or 
+               search(index+1,i,j+1,board,word) or
+               search(index+1,i-1,j,board,word) or
+               search(index+1,i,j-1,board,word);
+         
+        board[i][j] = word[index];
+     }
         
-        if(i<0 || j<0 || i>=board.size() || j>=board[0].size() || board[i][j] != word[idx]) return false;
-        
-        char temp = board[i][j];
-        board[i][j] = '*';
-        
-        bool res;
-        for(int k=0; k<4; k++)
-        {
-            int r=i+dir[k][0];
-            int c=j+dir[k][1];
-            
-            res = res || find(r, c, idx+1, board, word);
-        }
-        board[i][j] = temp;
-        
-        return res;
+     return ans;   
     }
-        
     
-    bool exist(vector<vector<char>>& board, string word) 
-    {
-        for(int i=0; i<board.size(); i++)
-        {
-            for(int j=0; j<board[0].size(); j++)
-            {
-                if(board[i][j] == word[0] && find(i, j, 0, board, word))
-                   return true;
-            }
-        }
-        return false;
+    
+    
+    
+    bool exist(vector<vector<char>>& board, string word) {
+      int m = board[0].size();
+      int n = board.size();
+      int index = 0; 
+      bool ans = false;
+     
+      for(int i=0; i<n ;i++)
+      {
+          for(int j = 0; j<m; j++)
+          {
+              if(word[index] == board[i][j])
+              {
+                if(search(index,i,j,board,word))  
+                {
+                    return true;
+                }
+              }
+          }
+      }
+        return ans;
     }
 };
+    
+//    bool find(int i,int j,int index,vector<vector<char>>& board, string word)
+//    {
+//        if(index==word.size())
+//            return true;
+//        if(i<0||j<0 ||i>=board.size()||j>=board.size())
+//            return false;
+//        bool ans = false;
+//        if(word[index]==board[i][j])
+//        {
+//            board[i][j]='#';
+//                ans = find(i+1,j,index+1,board,word)or
+//                      find(i,j+1,index+1,board,word)or
+//                      find(i-1,j,index+1,board,word)or
+//                      find(i,j-1,index+1,board,word);
+//            board[i][j]=word[index];
+//        }
+//        return ans;
+//    }
+    
+//     bool exist(vector<vector<char>>& board, string word) 
+//     {
+//         bool ans = false;
+//         int index=0;
+//         for(int i=0; i<board.size(); i++)
+//         {
+//             for(int j=0; j<board[0].size(); j++)
+//             {
+//                 if(board[i][j] == word[index])
+//                     if(find(i, j, index, board, word))
+//                         return true;
+//             }
+//         }
+//         return ans;
+//     }
+// };
