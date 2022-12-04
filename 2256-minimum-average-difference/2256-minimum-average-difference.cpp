1ad8bc13c7d26325377def1a11ed01a73b71cb24
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2022-12-05 01:36:53 +0530

    Time: 262 ms (59.00%), Space: 78.4 MB (76.57%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -1,32 +1,59 @@
+// class Solution {
+// public:
+//     int minimumAverageDifference(vector<int>& nums) {
+//         int n = nums.size();
+//         if(n==1)
+//             return 0;
+//         vector<int> v1;
+//         long long int sum1=0,sum=0,ans;
+//        vector<pair<long long int,long long int>>min;
+//         int j=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             sum+=nums[i];
+            
+//         }
+//         for(int i=0;i<n;i++)
+//         {
+//           sum1+=nums[i];
+//           sum-=nums[i];
+//           if(n-1-i==0)
+//           {
+//               j=1;
+//           }
+//             ans=abs(sum1/(i+1)-sum/(n-1-i-j));
+//             min.push_back(make_pair(ans,i));
+//         }
+//         sort(min.begin(),min.end());
+//         if(min.size()==1)
+//             return 0;
+//         return min[0].second;
+//     }
+// };
 class Solution {
 public:
     int minimumAverageDifference(vector<int>& nums) {
-        int n = nums.size();
-        if(n==1)
-            return 0;
-        vector<int> v1;
-        long long int sum1=0,sum=0,ans;
-       vector<pair<long long int,long long int>>min;
-        int j=0;
-        for(int i=0;i<n;i++)
+        long long i, n=nums.size(),sum1=0,sum2=0,minn=INT_MAX,avg,j;
+         
+        for(i=0;i<n;i++)
         {
-            sum+=nums[i];
-            
+            sum1+=nums[i];
         }
-        for(int i=0;i<n;i++)
-        {
-          sum1+=nums[i];
-          sum-=nums[i];
-          if(n-1-i==0)
+        cout<<sum1<<" "<<endl;
+        for(i=0;i<n;i++)
+        { sum2+=nums[i];
+          sum1-=nums[i];
+          if(i==n-1)
+          avg=sum2/n;
+          else
+          avg=abs((sum2/(i+1))-(sum1/(n-1-i)));
+          if(minn>avg)
           {
-              j=1;
+              minn=avg;
+              j=i;
           }
-            ans=abs(sum1/(i+1)-sum/(n-1-i-j));
-            min.push_back(make_pair(ans,i));
+        
         }
-        sort(min.begin(),min.end());
-        if(min.size()==1)
-            return 0;
-        return min[0].second;
+        return j;
     }
 };
