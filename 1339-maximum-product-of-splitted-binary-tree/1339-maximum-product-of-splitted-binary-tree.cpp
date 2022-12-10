2cd7224740efdf224d4791808ab92d687a9362fc
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2022-12-10 07:44:25 +0530

    Time: 309 ms (41.74%), Space: 79.7 MB (40.82%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -1,19 +1,21 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    vector<int> arr;
+class Solution
+{
+    public:
+        vector<int> arr;
 
-    int dfs(TreeNode* node) {
+    int dfs(TreeNode *node)
+    {
         if (not node) return 0;
         int cur = arr.size();
         arr.push_back(node->val);
@@ -21,11 +23,12 @@ public:
         return arr[cur];
     }
 
-    int maxProduct(TreeNode* root) {
+    int maxProduct(TreeNode *root)
+    {
         int modval = 1e9 + 7;
         long long ans = 0;
         dfs(root);
-        for (int i : arr) ans = max((long long) i * (arr[0] - i), ans);
+        for (int i: arr) ans = max((long long) i *(arr[0] - i), ans);
         return ans % modval;
     }
 };
