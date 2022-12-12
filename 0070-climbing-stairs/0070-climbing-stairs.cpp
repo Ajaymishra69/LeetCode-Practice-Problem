fe39bd5bf98ca42cf114f8524c4c9d7bd1bd8eaa
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2022-12-12 17:08:43 +0530

    Time: 4 ms (21.07%), Space: 5.9 MB (55.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,21 +1,29 @@
-class Solution {
-public:
-    int Cs(int n , int cnt,vector<int>&dp)
-    {
-        if(n<0)
-            return 0;
-        if(n==0)
-            return 1;
-        if(dp[n]!=-1)
-            return dp[n];
-       return dp[n]=(Cs(n-1,cnt+1,dp)+Cs(n-2,cnt+2,dp));
-        
-    }
-    int climbStairs(int n) {
-       
-       int cnt= 0;
-        vector<int>dp(n+1,-1);
-        cnt=Cs(n,cnt,dp);
-        return cnt;
-    }
+class Solution
+{
+    public:
+       	//     int Cs(int n, int cnt,vector<int>&dp)
+       	//     {
+       	//         if(n < 0)
+       	//             return 0;
+       	//         if(n==0)
+       	//             return 1;
+       	//         if(dp[n]!=-1)
+       	//             return dp[n];
+       	//        return dp[n]=(Cs(n-1,cnt+1,dp)+Cs(n-2,cnt+2,dp));
+
+       	//     }
+        int climbStairs(int n)
+        {
+            if (n == 1)
+                return 1;
+            int a = 2;
+            int b = 1;
+            for (int i = 3; i <= n; i++)
+            {
+                a = a + b;
+                b = a - b;
+            }
+
+            return a;
+        }
 };
