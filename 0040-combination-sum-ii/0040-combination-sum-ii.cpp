1fcd792591a7fd655040ff0a5062f729432cd558
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-01-29 02:27:12 +0530

    Time: 3 ms (92.89%), Space: 10.7 MB (68.84%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,41 +1,36 @@
-class Solution
-{
-    public:
-        void fun(int ind, int tar, vector<int> &candi,
-            vector<vector < int>> &res, vector< int > &temp)
-        {
-
-            if (tar <= 0 || ind == candi.size())
-            {
-                if (tar == 0)
-                    res.push_back(temp);	// Base Case
-                return;
-            }
-
-            for (int i = ind; i < candi.size(); i++)
-            {
-
-                if (i > ind && candi[i] == candi[i - 1])
-                    continue;
-
-                if (candi[i] > tar)
-                    break;
-
-                temp.push_back(candi[i]);
-                tar -= candi[i];
-
-                fun(i + 1, tar, candi, res, temp);
-
-                tar += candi[i];
-                temp.pop_back();
-            }
+class Solution {
+public:
+    void fun(int ind , int tar , vector<int>& candi ,
+            vector<vector<int>>& res , vector<int>& temp){
+        
+        if(tar<=0 || ind == candi.size()){
+            if(tar==0)
+                res.push_back(temp);            // Base Case
+            return;
         }
-    vector<vector < int>> combinationSum2(vector<int> &candi, int tar)
-    {
+        
+        for(int i = ind; i<candi.size();i++){
+          
+          if(i>ind && candi[i]==candi[i-1])
+              continue;
+          
+          if(candi[i]> tar)
+              break;
+          
+          temp.push_back(candi[i]);
+          tar-=candi[i];
+          
+          fun(i+1,tar,candi,res,temp);
+          
+          tar+=candi[i];  
+          temp.pop_back();
+      }   
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candi, int tar) {
         vector<int> temp;
-        vector<vector < int>> res;
+        vector<vector<int>>res;
         sort(candi.begin(), candi.end());
-        fun(0, tar, candi, res, temp);
+        fun(0,tar,candi,res,temp);
         return res;
     }
 };
