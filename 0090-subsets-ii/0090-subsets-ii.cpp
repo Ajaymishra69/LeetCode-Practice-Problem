905d1a09cf32dd0af570f8770110b22f0c912f14
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-01-30 00:29:21 +0530

    Time: 7 ms (40.32%), Space: 8.7 MB (29.18%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,38 +1,41 @@
-class Solution {
-public:
-     void fun(int ind, int n, vector<int> &nums,
+class Solution
+{
+    public:
+        void fun(int ind, int n, vector<int> &nums,
             vector<vector < int>> &ans, vector< int > &temp)
         {
             if (ind == n)
             {
-                ans.push_back(temp);        // Base Case
+                ans.push_back(temp);	// Base Case
                 return;
             }
-            temp.push_back(nums[ind]);         // pushing value temporarily to vector
+            temp.push_back(nums[ind]);	// pushing value temporarily to vector
 
-            fun(ind + 1, n, nums, ans, temp);   // when we are picking the number
+            fun(ind + 1, n, nums, ans, temp);	// when we are picking the number
 
-            temp.pop_back();             // if didn't pick the number pop the prev added num
+            temp.pop_back();	// if didn't pick the number pop the prev added num
 
-            fun(ind + 1, n, nums, ans, temp);       // When we aren't picking the num
+            fun(ind + 1, n, nums, ans, temp);	// When we aren't picking the num
         }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-         vector<vector < int>> ans;
-         vector<int> temp;
+    vector<vector < int>> subsetsWithDup(vector<int> &nums)
+    {
+        vector<vector < int>> ans;
+        vector<int> temp;
+        
         sort(nums.begin(), nums.end());
-         fun(0, nums.size(), nums, ans, temp);
         
-        set<vector<int>>st;
-        
-        for(auto it : ans)
+        fun(0, nums.size(), nums, ans, temp);
+
+        set<vector < int>> st;
+
+        for (auto it: ans)
             st.insert(it);
-        
+
         ans.clear();
-        
-        for(auto it : st)
+
+        for (auto it: st)
             ans.push_back(it);
-        
-        
-         return ans;
+
+        return ans;
     }
 };
