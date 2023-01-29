fa309dcce241da06225ada633517e2c898278cd4
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-01-30 00:21:08 +0530

    Time: 5 ms (41.18%), Space: 7.2 MB (71.57%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,23 +1,27 @@
-class Solution {
-public:
-    void fun(int ind , int n , vector<int>& nums , 
-             vector<vector<int>>& ans , vector<int>& temp){
-        if(ind == n){
-            ans.push_back(temp);
-            return;
+class Solution
+{
+    public:
+        void fun(int ind, int n, vector<int> &nums,
+            vector<vector < int>> &ans, vector< int > &temp)
+        {
+            if (ind == n)
+            {
+                ans.push_back(temp);        // Base Case
+                return;
+            }
+            temp.push_back(nums[ind]);         // pushing value temporarily to vector
+
+            fun(ind + 1, n, nums, ans, temp);   // when we are picking the number
+
+            temp.pop_back();             // if didn't pick the number pop the prev added num
+
+            fun(ind + 1, n, nums, ans, temp);       // When we aren't picking the num
         }
-        temp.push_back(nums[ind]);
-        
-        fun(ind+1 , n, nums,ans,temp);
-        
-        temp.pop_back();
-        
-        fun(ind+1 , n, nums,ans,temp);
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
+    vector<vector < int>> subsets(vector<int> &nums)
+    {
+        vector<vector < int>> ans;
         vector<int> temp;
-        fun(0,nums.size(),nums,ans,temp);
+        fun(0, nums.size(), nums, ans, temp);
         return ans;
     }
 };
