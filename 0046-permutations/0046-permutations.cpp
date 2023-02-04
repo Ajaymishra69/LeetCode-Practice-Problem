8827cc0d424bb25f76e9594a79d541b20c900906
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-02-04 15:54:06 +0530

    Time: 3 ms (77.27%), Space: 7.8 MB (57.16%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void fun(int n,vector<int>& nums,int freq[],vector<int>& v, vector<vector<int>>& ans){
+        if(v.size()==n){
+            ans.push_back(v);       //Base Case
+            return;
+        }
+        for(int i =0;i<n;i++){
+            if(freq[i]==0){
+                v.push_back(nums[i]);      //Recursive case
+                freq[i]=1;
+                fun(n,nums,freq,v,ans);
+                freq[i]=0;
+                v.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        int n = nums.size();
+        int freq[n];
+        vector<int> v;
+        vector<vector<int>>ans;
+        for(int i =0;i<n;i++){
+            freq[i]=0;
+        }
+        fun(n,nums,freq,v,ans);
+        return ans;
+    }
+};
