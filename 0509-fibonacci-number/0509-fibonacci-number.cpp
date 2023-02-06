55bbd0424ba5f170e7b186835c951e32aa794ace
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-02-06 14:48:15 +0530

    Time: 3 ms (69.65%), Space: 5.9 MB (69.00%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,18 @@
-class Solution {
-public:
-    int fib(int n) {
-        if(n==1 || n==0)
-            return n;
-        int prev2=0;
-        int prev1=1;
-        for(int i =2;i<n+1;i++)
+class Solution
+{
+    public:
+        int fib(int n)
         {
-            int curr = prev2+prev1;
-            prev2=prev1;
-            prev1=curr;
+            if (n == 1 || n == 0)
+                return n;
+            int prev2 = 0;
+            int prev1 = 1;
+            for (int i = 2; i < n + 1; i++)
+            {
+                int curr = prev2 + prev1;
+                prev2 = prev1;
+                prev1 = curr;
+            }
+            return prev1;
         }
-        return prev1;
-    }
 };
