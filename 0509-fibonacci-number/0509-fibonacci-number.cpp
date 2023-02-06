d6ba561694687a295873b715ef9c3fa1a97f7fbd
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-02-06 14:43:52 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (9.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,17 @@
-class Solution {
-public:
-    int fib(int n) {
-        if(n==1 || n==0)
-            return n;
-        vector<int>dp(n+1,-1);//Tabulation Method
-        dp[0]=0;
-        dp[1]=1;
-        for(int i =2;i<n+1;i++)
+class Solution
+{
+    public:
+        int fib(int n)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+            if (n == 1 || n == 0)
+                return n;
+            vector<int> dp(n + 1, -1);	//Tabulation Method
+            dp[0] = 0;
+            dp[1] = 1;
+            for (int i = 2; i < n + 1; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+            return dp[n];
         }
-        return dp[n];
-    }
 };
