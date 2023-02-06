9f3336f306a2dace8a8215d3edd229957386a452
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-02-06 14:47:30 +0530

    Time: 3 ms (69.65%), Space: 5.8 MB (99.90%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,7 +3,6 @@ public:
     int fib(int n) {
         if(n==1 || n==0)
             return n;
-        // Space optimization
         int prev2=0;
         int prev1=1;
         for(int i =2;i<n+1;i++)
