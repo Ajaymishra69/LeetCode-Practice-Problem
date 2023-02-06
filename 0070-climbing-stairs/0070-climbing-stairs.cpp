0985fe13880b883a83dc3a85547b6caf16d9cac6
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-02-06 15:22:49 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (17.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,18 @@
-class Solution
-{
-    public:
-        int climbStairs(int n)
-        {
-            if (n == 1)
-                return 1;
-            int a = 2;
-            int b = 1;
-            for (int i = 3; i <= n; i++)
-            {
-                a = a + b;
-                b = a - b;
-            }
-            return a;
-        }
+class Solution {
+public:
+    int f(int n , vector<int>& dp)
+    {
+        if(n<=1)
+            return 1;           //Base case
+        if(dp[n]!=-1)
+            return dp[n];
+        int left = f(n-1,dp);    // recurvise code
+        int right = f(n-2,dp);
+        return dp[n]=(left + right);
+    }
+    int climbStairs(int n) {
+        vector<int>dp(n+1,-1);
+        int ans = f(n,dp);
+        return ans;
+    }
 };
