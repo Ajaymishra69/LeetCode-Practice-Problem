559d05af83e7ad7f7085abde40365aaae0216c73
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-02-13 15:24:43 +0530

    Time: 5 ms (25.15%), Space: 7.8 MB (55.05%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,9 +5,7 @@ public:
         int prev2 = 0;
         int prev1 = nums[0];
         for(int i=1;i<n;i++){
-            int pick = nums[i];
-            if(i>1)
-                pick+=prev2;
+            int pick = nums[i]+prev2;
             int notpick = prev1;
             int curr = max(pick , notpick);
             prev2=prev1;
