a32225aa03a5c263fa894c17e3e2295803b7c4bd
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-02-13 14:54:24 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (30.73%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,19 +1,20 @@
-class Solution
-{
-    public:
-        int solve(vector<int> &nums, int index, vector<int> &dp)
-        {
-            if (index >= nums.size())
-                return 0;
-            if (dp[index] != -1)
-                return dp[index];
-            return dp[index] = max(solve(nums, index + 1, dp), nums[index] + solve(nums, index + 2, dp));
-        }
-
-    int rob(vector<int> &nums)
-    {
-        int n = nums.size();
-        vector<int> dp(n + 1, -1);
-        return solve(nums, 0, dp);
+class Solution {
+public:
+    int f(vector<int>& nums, int ind ,vector<int>& dp){
+        if(ind == 0)
+            return nums[ind];
+        if(ind<0)
+            return 0;
+        if(dp[ind]!=-1)
+            return dp[ind];
+        int pick = f(nums,ind-2,dp)+nums[ind];
+        int notpick = f(nums,ind-1,dp);
+        return dp[ind] = max(pick,notpick);
+    }
+    int rob(vector<int>& nums) {
+        
+        vector<int>dp(nums.size()+1,-1);
+        
+        return f(nums,nums.size()-1,dp);
     }
 };
