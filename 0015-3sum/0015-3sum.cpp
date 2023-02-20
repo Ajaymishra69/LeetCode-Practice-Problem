bb5718159d4c382c2ed742786bf2176a5957a96e
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-02-20 16:49:46 +0530

    Time: 345 ms (28.94%), Space: 46.7 MB (19.66%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> ans;
-    sort(nums.begin(), nums.end());
-    // nums[i]+nums[j]+nums[k]=0
-    for (int i = 0; i < nums.size() - 2; ++i)
-    {
-        if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
-        {
-            int lo = i + 1, hi = nums.size() - 1;
-            int target = -nums[i];
-            while (lo < hi)
-            {
-                if (nums[lo] + nums[hi] == target)
-                {
-                    vector<int> temp;
-                    temp = {nums[i], nums[lo], nums[hi]};
-                    ans.push_back(temp);
-                    while (lo < hi && nums[lo] == nums[lo + 1])
+       sort(nums.begin(),nums.end());
+        int n = nums.size();
+        vector<vector<int>>res;
+        set<vector<int>>st;
+        for(int i=0;i<n-1;i++){
+            int lo = i+1,hi=n-1;
+            int tar = 0-nums[i];
+            while(lo<hi){
+                if(nums[lo]+nums[hi] == tar){
+                    vector<int>temp;
+                    temp = {nums[i],nums[lo],nums[hi]};
+                    res.push_back(temp);
+                    while(lo<hi && nums[lo]==nums[lo+1])
                     {
                         lo++;
                     }
-                    while (lo < hi && nums[hi] == nums[hi - 1])
+                     while(lo<hi && nums[hi]==nums[hi-1])
                     {
                         hi--;
                     }
-                    lo++, hi--;
+                    lo++,hi--;
                 }
-                else if(nums[lo]+nums[hi]<target){
-                    lo++;
-                }
-                else{
+                else if(nums[lo]+nums[hi] >tar)
                     hi--;
-                }
+                else
+                    lo++;
             }
         }
-    }
-    return ans;
+        for(auto it:res)
+            st.insert(it);
+        res.clear();
+        for(auto it:st)
+            res.push_back(it);
+        return res;
     }
 };
