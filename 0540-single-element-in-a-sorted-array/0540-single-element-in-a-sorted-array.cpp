a08a0e10439e531559888ee584506682ad076ed4
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-02-21 14:59:31 +0530

    Time: 34 ms (32.30%), Space: 22.5 MB (16.37%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,27 +1,24 @@
-class Solution
-{
-    public:
-        int singleNonDuplicate(vector<int> &nums)
-        {
-            int n = nums.size();
-            int s = 0;
-            int e = n - 1;
-            if (n == 1)
-                return nums[0];
-            if (nums[0] != nums[1])
-                return nums[0];
-            if (nums[e] != nums[e - 1])
-                return nums[e];
-            while (s <= e)
-            {
-                int mid = s + (e - s) / 2;
-                if ((nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) && (mid > 0 && mid < n - 1))
-                    return nums[mid];
-                if ((nums[mid] == nums[mid + 1] && mid % 2 == 0) || (nums[mid] == nums[mid - 1] && mid % 2 != 0))
-                    s = mid + 1;
-                else
-                    e = mid - 1;
-            }
-            return nums[s];
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int s = 0;
+        int e = n-1;
+        if(n==1)
+        return nums[0];
+        if(nums[0]!=nums[1])
+        return nums[0];
+        if(nums[e]!=nums[e-1])
+        return nums[e];
+        while(s<=e){
+            int  mid =s+(e-s)/2;
+            if((nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) && (mid>0 && mid<n-1))
+                return nums[mid];
+            if((nums[mid]==nums[mid+1] && mid%2==0)||(nums[mid]==nums[mid-1] && mid%2!=0))
+                 s=mid+1;
+            else
+                e=mid-1;
         }
+        return nums[s];
+    }
 };
