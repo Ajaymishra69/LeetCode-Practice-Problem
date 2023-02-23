1e38366379b81d4813faf4dcf78e1f63f5a580fa
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-02-23 17:53:10 +0530

    Time: 20 ms (38.63%), Space: 12.6 MB (17.19%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,24 +1,28 @@
-class Solution {
-public:
-    vector<string> findRelativeRanks(vector<int>& score) {
-        vector<int>s;
-        vector<string>ans;
-        s=score;
-        unordered_map<int,string>v;
-        sort(s.begin(),s.end(),greater<int>());
-        for(int i=0;i<s.size();i++){
-            if(i==0)
-                v[s[i]]="Gold Medal";
-            else if(i==1)
-                v[s[i]]="Silver Medal";
-            else if(i==2)
-              v[s[i]]="Bronze Medal";
-            else
-                v[s[i]]=to_string(i+1);
+class Solution
+{
+    public:
+        vector<string> findRelativeRanks(vector<int> &score)
+        {
+            vector<int> s;
+            vector<string> ans;
+            s = score;
+            unordered_map<int, string> v;
+            sort(s.begin(), s.end(), greater<int> ());
+            for (int i = 0; i < s.size(); i++)
+            {
+                if (i == 0)
+                    v[s[i]] = "Gold Medal";
+                else if (i == 1)
+                    v[s[i]] = "Silver Medal";
+                else if (i == 2)
+                    v[s[i]] = "Bronze Medal";
+                else
+                    v[s[i]] = to_string(i + 1);
+            }
+            for (auto i: score)
+            {
+                ans.push_back(v[i]);
+            }
+            return ans;
         }
-        for(auto i:score){
-            ans.push_back(v[i]);
-        }
-        return ans;
-    }
 };
