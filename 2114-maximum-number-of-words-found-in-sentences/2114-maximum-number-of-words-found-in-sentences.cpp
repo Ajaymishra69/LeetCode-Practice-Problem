f9112844d583110cc876ac333809216a866a451d
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-02-28 19:23:22 +0530

    Time: 11 ms (82.88%), Space: 10.3 MB (15.57%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int mostWordsFound(vector<string>& sen) {
+        //we use (int) becoz count func returns a unsigned value
+        // maxi+1 becoz no of words should be 1 more than spaces
         int maxi = INT_MIN;
         for(auto it:sen)
             maxi=max(maxi,(int)count(it.begin(),it.end(),' '));
