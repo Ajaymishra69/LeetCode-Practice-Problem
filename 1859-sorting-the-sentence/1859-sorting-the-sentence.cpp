797068213a38fa20fb6f647234f22d110f0af2f1
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-03-02 00:29:10 +0530

    Time: 2 ms (43.74%), Space: 6.4 MB (13.73%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        vector<pair<int,string>>v;
+        string ans="";
+        istringstream ss(s);
+        string temp;
+        while(ss>>temp){
+            int n = temp.size();
+            int x = temp[n-1];
+            temp.pop_back();
+            v.push_back({x,temp});
+        }
+        sort(v.begin(),v.end());
+        for(auto i:v)
+        {
+            ans+=i.second+" ";
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
