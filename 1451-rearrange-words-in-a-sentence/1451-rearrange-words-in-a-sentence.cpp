101a40625da0d98315513458765a0737f6948a5e
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-03-17 00:10:15 +0530

    Time: 46 ms (19.47%), Space: 18.5 MB (39.60%) - LeetHub

--- 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.cpp
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.cpp
@@ -3,12 +3,10 @@ public:
     string arrangeWords(string text) {
         multimap<int,string>mp;
         string ans = "";
-        //transform(text.begin(),text.end(),text.begin(),::tolower);
+        transform(text.begin(),text.end(),text.begin(),::tolower);
         istringstream ss(text);
         string str;
         while(ss>>str){
-            if(str[0]>=65&&str[0]<97)
-                str[0]=str[0]+32;
             int n = str.size();
             mp.insert(pair<int,string>(n,str));
         }
