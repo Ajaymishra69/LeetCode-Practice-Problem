1530ce5e677e6323bb75204b3da2a511e97c194c
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-03-19 01:52:52 +0530

    Time: 11 ms (22.91%), Space: 11.7 MB (8.47%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -4,7 +4,6 @@ public:
         unordered_map<int,int>mp;
         for(auto i:nums)
             mp[i]++;
-        // for(auto i:mp)cout<<i.first<<":"<<i.second<<endl;
         nums.clear();
         for(auto i:mp){
             if(i.second<=2){
