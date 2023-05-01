b0b5e0350e272a078349f8e41884128b58c835ce
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-05-01 13:51:45 +0530

    Time: 5 ms (23.02%), Space: 7 MB (84.06%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     double average(vector<int>& salary) {
-        double avg=0.00;
         int n=salary.size();
         sort(salary.begin(),salary.end());
+        double ans;
         for(int i=1;i<n-1;i++)
-        {
-            avg=avg+salary[i];
-        }
-        return (avg/(n-2));
+            ans+=salary[i];
+        ans=ans/(n-2);
+        return ans;
     }
 };
