107f893eb960a05be485d9510d89958dd4fcfd52
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-05-17 10:44:50 +0530

    Time: 5 ms (32.78%), Space: 7.1 MB (71.30%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -1,43 +1,26 @@
 /**
- *Definition for singly-linked list.
- *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
- *};
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
-class Solution
-{
-    public:
-        ListNode* middleNode(ListNode *head)
-        {
-            if (head->next == NULL)
-                return head;
-            int cnt = 0;
-            ListNode *temp = head;
-            while (temp != NULL)
-            {
-                temp = temp->next;
-                cnt++;
-            }
-           //	temp=head;
-            if (cnt % 2 == 0)
-            {
-               for(int i = 0;i<(cnt/2);i++)
-               {
-                   head =  head->next;
-               }
-            }
-            else
-            {
-               for(int i = 0;i<(cnt/2);i++)
-               {
-                   head =  head->next;
-               }
-            }
-
-            return head;
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* temp = head;
+        int n = 0;
+        while(temp!=NULL){
+            temp=temp->next;
+            n++;
         }
+        n = n/2;
+        while(n--){
+            head = head->next;
+        }
+        return head;
+    }
 };
