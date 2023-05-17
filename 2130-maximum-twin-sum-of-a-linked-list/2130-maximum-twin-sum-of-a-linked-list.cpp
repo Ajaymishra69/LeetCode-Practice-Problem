19e694f28abd67ed1203333c9895cd111b5f59a0
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-05-17 10:13:37 +0530

    Time: 294 ms (14.21%), Space: 126.7 MB (23.20%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,39 +1,28 @@
 /**
- *Definition for singly-linked list.
- *struct ListNode {
- *    int val;
- *    ListNode * next;
- *    ListNode() : val(0), next(nullptr) {}
- *    ListNode(int x) : val(x), next(nullptr) {}
- *    ListNode(int x, ListNode *next) : val(x), next(next) {}
- *};
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
  */
-class Solution
-{
-    public:
-        int pairSum(ListNode *head)
-        {
-            if (head->next->next == NULL)
-            {
-                return (head->val + head->next->val);
-            }
-            vector<pair<int, int>>p1;
-            int ans =0,max = INT_MIN;
-            int i =0;
-            while(head!=NULL)
-            {
-                p1.push_back(make_pair(i,head->val));
-                head=head->next;
-                i++;
-            }
-            for(int j =0;j<i/2;j++)
-            {
-                ans=p1[j].second+p1[i-1-j].second;
-                if(ans>max)
-                {
-                    max=ans;
-                }
-            }
-            return max;
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        ListNode* temp = head;
+        vector<int>v;
+        int maxi = -1;
+        while(temp!=NULL){
+            v.push_back(temp->val);
+            temp=temp->next;
         }
+        int n = v.size();
+        for(int i =0;i<n/2;i++){
+            if(v[i]+v[n-i-1]>maxi)
+                maxi=v[i]+v[n-i-1];
+        }
+        return maxi;
+    }
 };
