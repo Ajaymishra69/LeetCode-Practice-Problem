2f8a05af3506885b701231bf726fd93533ddcfc7
Ajay Mishra <86868065+Ajaymishra69@users.noreply.github.com>
2023-11-23 13:26:21 +0530

    Time: 78 ms (18.67%), Space: 39 MB (24.84%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1630-arithmetic-subarrays/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1630-arithmetic-subarrays.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1630-arithmetic-subarrays/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divbloomberg-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div1630-arithmetic-subarrays.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     bool check(vector<int>&temp){
         int n = temp.size();
-        //cout<<"0"<<endl;
-        for(int i =0;i<n-1;i++){
-            if(temp[i+1] - temp[i] != temp[1] - temp[0])
-                break;
-        }
-       // cout<<"1"<<endl;
-        sort(temp.begin(),temp.end());
-         for(int i =0;i<n-1;i++){
-            if(temp[i+1] - temp[i] != temp[1] - temp[0])
-                break;
-        }
-       // cout<<"2"<<endl;
+       //  //cout<<"0"<<endl;
+       //  for(int i =0;i<n-1;i++){
+       //      if(temp[i+1] - temp[i] != temp[1] - temp[0])
+       //          break;
+       //  }
+       // // cout<<"1"<<endl;
+       //  sort(temp.begin(),temp.end());
+       //   for(int i =0;i<n-1;i++){
+       //      if(temp[i+1] - temp[i] != temp[1] - temp[0])
+       //          break;
+       //  }
+       // // cout<<"2"<<endl;
         sort(temp.begin(),temp.end(),greater<int>());
          for(int i =0;i<n-1;i++){
             if(temp[i+1] - temp[i] != temp[1] - temp[0])
